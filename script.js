9401adb20f57cf7449c74eec7c58f606f0cc73d4
JHEISON ESTIBEN <95143921+jheisonGZ@users.noreply.github.com>
2022-08-24 23:45:46 -0500

    Inicial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+function descargar(){
+    const dowloat=document.createElement('a');
+    dowloat.href = 'img/cv.pdf';
+    //dowloat.target = '_blank';
+    dowloat.download = "Estiven";
+    document.body.appendChild(dowloat);
+    dowloat.click();
+    document.body.removeChild(dowloat);
+ }
+
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    //hacemos desaparecer el menu una vez que se ha seleccionado una opcion en el modo responsive
+   var x = document.getElementById("nav");
+   x.className = "";
+}
+
+//función que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades()
+};
+
+//funcion que aplica la animacion de la barra de habilidades 
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >=300){
+       document.getElementById("html").classList.add("barra-progreso1");
+       document.getElementById("js").classList.add("barra-progreso2");
+       document.getElementById("bd").classList.add("barra-progreso3");
+       document.getElementById("py").classList.add("barra-progreso4");
+
+
+    }
+    
+}
